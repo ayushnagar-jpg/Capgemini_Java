5ab7dfa8d6b4526ec741c8837918b70786bcca80
Ayush Nagar <ayushnagar633@gmail.com>
2025-02-07 14:12:53 +0530

    [AYUSH]ADD:Problem solving practice

---
+++ LineComparison.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class LineComparison{
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.nextLine();
+        String s2 = sc.nextLine();
+
+        if(s1.length() > s2.length()){
+            System.out.println("s1 is greater");
+        }
+        else if(s1.length() < s2.length()){
+            System.out.println("s2 is Greater");
+        }
+        else{
+            System.out.println("Both are Equal");
+        }
+    }
+
+    
+    
+}
---
+++ WordReplace.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class WordReplace {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Word to be replaced");
+        String s = sc.nextLine();
+        System.out.println("Enter the position to replace the word");
+        int p = sc.nextInt();
+        String word = sc.next();
+        String[]wordArray = s.split(" ");
+        for (int i = 0; i < 10; i++) {
+            
+        }
+
+    }
+}
